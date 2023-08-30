2eae219e9d624350d17c8aa7e38db6de15ef8eb1
EmanuelaPau <uapame@gmail.com>
2023-08-30 15:36:00 +0200

    Add vue project

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,271 +1,34 @@
+
 <template>
-    <footer class="">
-        <!-- About Us  -->
-        <div class=" ">
-            <div class="container-fluid footer-about">
-                <div class="row my_container">
-                    <div class="col-12 col-md-6 col-lg-3 px-3">
-                        <h1 class="my_h1">
-                            About Us
-                        </h1>
-                        <p class="my_p">
-                            Mauris malesuada arcu eu posuere eleifen. Ut egestas tortor at leo egestas tincidunt. Nam
-                            tincidunt metus tristique tristique viverra. Mauris erat ante, egestas vitae dapibus seds.
-                        </p>
-                        <ul class="list-unstyled d-flex flex-wrap">
-                            <li v-for="icon in    socialIcons   " class="me-3 social-icon" :class="icon.color">
-                                <a :class="icon.icon" href="">
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-12 col-md-6 col-lg-3 px-3">
-                        <h1 class="my_h1">
-                            Movie Category
-                        </h1>
-                        <ul class="list-unstyled">
-                            <li v-for="li in movieCategory" class="footer-li">
-                                <a class="text-decoration-none text-reset" href="">{{ li.name }}</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-12 col-md-6 col-lg-3 px-3">
-                        <h1 class="my_h1">
-                            Information
-                        </h1>
-                        <ul class="list-unstyled">
-                            <li v-for="li in information" class="footer-li">
-                                <a class="text-decoration-none text-reset" href="">{{ li.name }}</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-3 px-3">
-                        <h1 class="my_h1">
-                            Recent Posts
-                        </h1>
-                        <div v-for="cards in recentPosts" class="footer-card d-flex">
-                            <img :src="cards.img" alt="">
-                            <div class="d-flex flex-wrap align-items-end">
-                                <p class="my_p m-0 card-title">{{ cards.title }}</p>
-                                <p class="my_p m-0">{{ cards.date }}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-        <div class="footer-about-back"></div>
-        <!-- Footer bottom  -->
-        <div class="container-fluid">
-            <div class="row my_container py-3">
-                <div class="col-6">
-                    <ul class="d-flex flex-wrap list-unstyled my_nav m-0">
-                        <li v-for="   element    in    footerBottomNav   " class="me-4">
-                            <a class="text-reset text-decoration-none" href="">
-                                {{ element }}
-                            </a>
-                        </li>
-
-                    </ul>
-                </div>
-                <div class="col-6 d-flex flex-wrap justify-content-end px-0">
-                    <span class="me-1">
-                        © 2020
-                    </span>
-                    <span class="txtColorPrimary me-1 my_info">
-                        <a class="" href="">@Themepul</a>
-                    </span>
-                    <span>
-                        All Rights Reserved.
-                    </span>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <div>
+        FOOTER
+    </div>
 </template>
 
 <script>
+// < !-- import HelloWorld from './HelloWorld.vue' -->
 
 export default {
     name: 'AppFooter',
-    props: {
-        aboutUs: 'Object',
-        movieCategory: 'Object',
-        information: 'Object',
-        recentPosts: 'Object',
-        footerBottomNav: 'Object',
-        socialIcons: 'Array',
-    },
     components: {
     },
     data() {
         return {
+
             // links: [
             //	'gino',
             //	'ciao',
-            //	]
+            //	]       
+
         }
     },
     methods() {
     },
-}		
+}	
 </script>
 <style scoped lang="scss">
-@use '../styles/general.scss';
 @use '../styles/partials/_variables.scss';
 @use '../styles/partials/_mixins.scss';
-
-.footer-about {
-    padding-top: 70px;
-    width: 100%;
-    line-height: 24px;
-    position: relative;
-    z-index: 1;
-}
-
-div.footer-about:before {
-    content: '';
-    height: 100%;
-    width: 100%;
-    background-image: url('ft-bg.jpg');
-    background-size: cover;
-    background-position: center;
-    filter: grayscale(100%);
-    opacity: 0.1;
-    z-index: 0;
-
-    position: absolute;
-    top: 0;
-    right: 0;
-}
-
-.footer-li {
-    height: 48px;
-    font-size: 15px;
-    transition: all .8s;
-    z-index: 1;
-}
-
-.footer-li:hover {
-    color: #13be13;
-}
-
-.footer-card {
-    width: 100%;
-    height: 85px;
-    margin-bottom: 30px;
-    z-index: 1;
-
-    img {
-        height: 100%;
-        margin-right: 10px;
-        z-index: 1;
-    }
-}
-
-.my_nav li {
-    transition: all .8s;
-    z-index: 1;
-}
-
-.my_nav li:hover {
-    color: #13be13;
-}
-
-.my_info a {
-    transition: all .8s;
-    color: #13be13;
-    z-index: 1;
-}
-
-.my_info a:hover {
-    color: white;
-}
-
-h1.my_h1 {
-    position: relative;
-    font-size: 18px;
-    margin-bottom: 40px;
-    z-index: 1;
-}
-
-h1.my_h1:after {
-    content: "";
-    position: absolute;
-    top: 20px;
-    left: 0;
-    height: 10px;
-    width: 30px;
-    border-bottom: 2px solid white;
-    z-index: 1;
-}
-
-p.my_p {
-    font-size: 14px;
-    z-index: 1;
-    z-index: 1;
-}
-
-.social-icon {
-    height: 32px;
-    width: 32px;
-    border-radius: 50%;
-    cursor: pointer;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #13be13;
-    transition: all .7s;
-
-    a {
-        color: white;
-        text-decoration: none;
-    }
-
-    z-index: 1;
-}
-
-.social-icon:hover {
-    background-color: transparent;
-    color: #13be13;
-
-    a {
-        color: #13be13;
-    }
-}
-
-.card-title {
-    width: 200px;
-    transition: all .7s;
-    cursor: pointer;
-    z-index: 1;
-}
-
-.card-title:hover {
-    color: #13be13;
-}
-
-// Colors 
-
-.fb-color {
-    background-color: #02b0e8;
-}
-
-.tw-color {
-    background-color: #02b0e8;
-}
-
-.ln-color {
-    background-color: #00a098;
-}
-
-.ig-color {
-    background-color: #c22e2a;
-}
-
-.pn-color {
-    background-color: #b00a1b;
-}
 </style> 
+
+    
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,246 +1,36 @@
+
 <template>
-    <div class="">
-        <!-- Header top -->
-        <div class="line">
-            <div class="container-fluid">
-                <div class="row py-3 my_container m-auto d-flex flex-wrap justify-content-between">
-
-                    <!-- Mail -->
-                    <div class="header-mail col-lg-3 col-6">
-                        <i class="fa-regular fa-envelope me-1"></i>
-                        <span>Superhit Top Movie </span>
-                        <span class="txtColorPrimary">** King Star **</span>
-                    </div>
-                    <!-- Socials buttons -->
-                    <div class="header-socials col-lg-8 col-md-3 col-6 d-flex justify-content-end">
-                        <a class="text-reset " href=""><i class="fa-brands fa-facebook-f mx-2 facebook"></i></a>
-                        <a class="text-reset " href=""> <i class="fa-brands fa-twitter mx-2 twitter"></i></a>
-                        <a class="text-reset " href=""><i class="fa-brands fa-linkedin-in mx-2 linkedin"></i></a>
-                        <a class="text-reset " href=""><i class="fa-brands fa-instagram mx-2 instagram"></i></a>
-                        <a class="text-reset " href=""><i class="fa-brands fa-google-plus-g mx-2 google"></i></a>
-                    </div>
-
-                    <!-- Login -->
-                    <div class="header-login col-lg-1 d-none d-lg-block">
-                        <a class="text-reset text-decoration-none" href="#">
-                            <i class="fa-solid fa-lock me-2"></i>
-                            <span>Login</span>
-                        </a>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        <!-- header search  -->
-        <div class="header-search line py-3">
-            <div class="container-fluid">
-                <div
-                    class="row my_container m-auto d-flex flex-column flex-lg-row justify-content-center align-items-center">
-                    <div class="col-5 d-flex justify-content-center justify-content-lg-start ">
-                        <img id="my_logo" src="cropped-logo.png" alt="">
-                    </div>
-                    <div class="col-7 py-3 px-0 d-flex d-none d-md-flex justify-content-center justify-content-lg-end">
-                        <div class="my_searchbar d-flex">
-                            <input class="my_input bgColorSecondary" type="search" placeholder="Search ..."
-                                aria-label="Search">
-                            <button class="my_search-button bgColorPrimary" type="submit">Search</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- header nav -->
-        <div class=" my_nav-font px-3 px-lg-0 ">
-            <nav class="my_container navbar navbar-expand-lg py-4 ">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li v-for="dropDown in navDropdown" class="nav-item dropdown show position-relative"
-                            @mouseover="$emit('show', dropDown)" @mouseleave="$emit('hide', dropDown)">
-                            <a class="text-reset text-decoration-none dropdown-toggle me-3" href="#" role="button"
-                                id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                {{ dropDown.name }}
-                            </a>
-
-                            <div class="my_dropdown-menu position-absolute"
-                                :class="dropDown.state == 'hide' ? 'd-none' : 'd-block'" aria-labelledby="dropdownMenuLink">
-                                <a v-for="element in dropDown.list" class="dropdown-item mb-2 my_dd-item" href="#">{{
-                                    element }}</a>
-
-                            </div>
-                        </li>
-                        <li v-for="nav in navLi" class="nav-item">
-                            <a class="text-reset text-decoration-none me-3" :href="nav.link">{{ nav.name }}</a>
-                        </li>
-
-                    </ul>
-                </div>
-                <div class="position-relative">
-                    <div class="basket-items-notification bgColorPrimary">
-                        <p class="m-0 text-center m-auto">0</p>
-                    </div>
-                    <i class="fa-solid fa-basket-shopping basket-icon">
-                    </i>
-                </div>
-            </nav>
-
-        </div>
+    <div>
+        <p>
+            HEADER
+        </p>
     </div>
 </template>
 
 <script>
-//  import HelloWorld from './HelloWorld.vue'
+// < !-- import HelloWorld from './HelloWorld.vue' -->
 
 export default {
     name: 'AppHeader',
-    props: {
-        navDropdown: 'Array',
-        navLi: 'Array',
-    },
     components: {
     },
     data() {
         return {
 
+            // links: [
+            //	'gino',
+            //	'ciao',
+            //	]       
+
         }
     },
     methods() {
     },
-
-}		
+}	
 </script>
 <style scoped lang="scss">
-@use '../styles/partials/variables.scss';
+@use '../styles/partials/_variables.scss';
 @use '../styles/partials/_mixins.scss';
-
-div.line {
-    border-bottom: 1px solid #2b333d;
-}
-
-.header-mail {
-
-    span,
-    i {
-        font-size: 14px;
-    }
-}
-
-.header-socials {
-
-    i.facebook:hover,
-    i.twitter:hover,
-    i.linkedin:hover,
-    i.instagram:hover,
-    i.google:hover {
-        color: #13be13;
-
-    }
-
-    i {
-        transition: all .8s;
-        font-size: 14px;
-    }
-
-    i.facebook {
-        color: #3b5998;
-    }
-
-    i.twitter {
-        color: #00acee;
-    }
-
-    i.linkedin {
-        color: #0e76a8;
-    }
-
-    i.instagram {
-        color: #c8232c;
-    }
-
-    i.google {
-        color: #db4a39;
-    }
-}
-
-.header-login {
-
-    i,
-    span {
-        font-size: 14px;
-    }
-}
-
-img#my_logo {
-    width: 185px;
-    cursor: pointers;
-}
-
-div.header-search {
-    // height: 125px;
-
-    .my_searchbar {
-        height: 50px;
-
-        .my_input {
-            width: 250px;
-            color: #2b333d;
-            border: 1px solid #2b333d;
-            border-left: none;
-            padding: 1rem;
-        }
-
-        .my_search-button {
-            border: none;
-            width: 85px;
-            color: white;
-            font-weight: 600;
-        }
-    }
-}
-
-.my_nav-font {
-    font-size: 18px !important;
-    font-weight: 700 !important;
-}
-
-.my_dd-item {
-    font-size: 14px;
-    transition: all .3s;
-}
-
-.my_dd-item:hover {
-    color: #13be13;
-}
-
-.basket-items-notification {
-    font-size: 12px;
-    border-radius: 50%;
-    height: 16px;
-    width: 16px;
-    display: flex;
-
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translateX(50%) translateY(-50%);
-}
-
-.basket-icon {
-    font-size: 15px;
-}
-
-.navbar-nav li {
-    margin-right: 11px;
-}
-
-.my_dropdown-menu {
-    z-index: 1;
-    width: 200px;
-    background-color: #060f19;
-    padding: 40px 10px 20px 10px;
-}
 </style> 
+
+    
